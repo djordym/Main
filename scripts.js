f8ad630988265df26c54ae6316988eb19e51cafa
djordym <131238624+djordym@users.noreply.github.com>
2023-04-26 09:24:06 +0200

    Update plant names

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const images = [    "Aloe_Vera.jpg",    "Chinese_monkey_plant.jpg",    "devilsvine.jpg",    "elephant_cactus.jpg",    "Euphorbia_lactea_cristata.jpg",     "monstera_deliciosa.jpg",    "Moon_Valley_Pilea.jpg",    "navvine_500.png",    "Peperomia_Obtusifolia.jpg",    "Peperomia_Polybotrya.jpg",    "Prayer_Plant.jpg",    "Rex_Begonia.jpg",    "Silver_Inch_Plant.jpg",     "umbrella_plant.jpg"];
+const images = [    "Aloe Vera.jpg",    "Chinese_monkey_plant.jpg",    "devilsvine.jpg",    "elephant_cactus.jpg",    "Euphorbia_lactea_cristata.jpg",     "monstera_deliciosa.jpg",    "Moon_Valley_Pilea.jpg",        "Peperomia_Obtusifolia.jpg",    "Peperomia_Polybotrya.jpg",    "Prayer_Plant.jpg",    "Rex_Begonia.jpg",    "Silver_Inch_Plant.jpg",     "umbrella_plant.jpg"];
 
 
 const setup = () => {
@@ -6,7 +6,14 @@ const setup = () => {
   let ImgName = document.querySelector("#gallery");
 
   for (i = 0; i < images.length; i++) {
-    ImgName.insertAdjacentHTML("beforeend", `<img class="galpic" src="images/${images[i]}"></img>`);
+    ImgName.insertAdjacentHTML("beforeend", 
+    `
+    <div class="frame">
+    <img class="image" src="images/${images[i]}"></img>
+    <p>
+    </div>
+    
+    `);
   }
 
 }
