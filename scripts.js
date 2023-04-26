44723363d4c2d45ff231e832bf00fbe7005c36a4
djordym <131238624+djordym@users.noreply.github.com>
2023-04-26 10:40:44 +0200

    v0.1

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
-const images = [    "Aloe Vera.jpg",    "Chinese_monkey_plant.jpg",    "devilsvine.jpg",    "elephant_cactus.jpg",    "Euphorbia_lactea_cristata.jpg",     "monstera_deliciosa.jpg",    "Moon_Valley_Pilea.jpg",        "Peperomia_Obtusifolia.jpg",    "Peperomia_Polybotrya.jpg",    "Prayer_Plant.jpg",    "Rex_Begonia.jpg",    "Silver_Inch_Plant.jpg",     "umbrella_plant.jpg"];
+const images = [  "Aloe Vera.jpg",  "Baby Rubberplant.jpg",  "Chinese Money Plant.jpg",  "Devil's Vine.jpg",  "Elephant Cactus.jpg",  "Euphorbia Lactea Cristata.jpg",  "Monstera Deliciosa.jpg",  "Moon Valley Pilea.jpg",  "Prayer Plant.jpg",  "Raindrop Plant.jpg",  "Rex Begonia.jpg",  "Silver Inch Plant.jpg",  "Umbrella Plant.jpg"]
 
 
 const setup = () => {
 
   let ImgName = document.querySelector("#gallery");
 
+
   for (i = 0; i < images.length; i++) {
+    
     ImgName.insertAdjacentHTML("beforeend", 
     `
     <div class="frame">
     <img class="image" src="images/${images[i]}"></img>
-    <p>
+    <p>${images[i].slice(0,-4)}</p>
     </div>
     
     `);
@@ -19,3 +21,7 @@ const setup = () => {
 }
 
 window.addEventListener("load", setup);
+
+//let plantName = i;
+//    plantName = plantName.slice(0, -1);
+//    <p>${plantName}</p>
